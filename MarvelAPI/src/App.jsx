f2d93bbfa9cbe2c1f44bd0d3f2c39655663e1739
Kevsp178 <kespejo2021@fau.edu>
2024-03-20 20:14:22 -0400

    API working

--- MarvelAPI/src/App.jsx
+++ MarvelAPI/src/App.jsx
@@ -4,12 +4,37 @@ import "./App.css";
 import MarvelInfo from "./Components/Marvel";
 
 function App() {
-  //const url =
-  // "https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&city=Miami,FL&start_date=2024-03-16&end_date=2024-03-17&key=f88cd683b08c43f6b114fa83ec1375f9";
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  https: return (
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      const url = `https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&city=${location}&start_date=2024-03-16&end_date=2024-03-17&key=f88cd683b08c43f6b114fa83ec1375f9`;
+      axios
+        .get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+        });
+      setLocation(""); // Clear the input field after making the API call
+    }
+  };
+
+  return (
     <div className='app'>
       <div className='container'>
+        <div className='search'>
+          <input
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            placeholder='Enter Location'
+            onKeyPress={searchLocation}
+            type='text'
+          />
+        </div>
         <div className='top'>
           <div className='location'>
             <p>Miami</p>
